ade62a235f3c40f1b74e30c4a582953727d586e8
briceyokoyama <briceyokoyama@gmail.com>
2019-11-11 21:19:33 -0800

    add localStorage for todos

--- src/todo_list.js
+++ src/todo_list.js
@@ -1,6 +1,7 @@
-const todos = []
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 const ulElement = document.getElementsByClassName('todos')[0];
 const formElement = document.getElementsByClassName('add-todo-form')[0];
+const divElement = document.getElementsByClassName('todo-container')[0];
 
 function addTodo(e) {
   e.preventDefault();
@@ -18,7 +19,7 @@ function populateList(todos) {
 
   Array(...ulElement.children).forEach(child => child.remove());
 
-  todos.forEach(todo => {
+  todos.forEach((todo, index) => {
     let labelElement = document.createElement('label');
     let checkboxElement = document.createElement('input');
     let listElement = document.createElement('li');
@@ -27,14 +28,24 @@ function populateList(todos) {
 
     checkboxElement.type = 'checkbox';
     checkboxElement.checked = todo.done;
+    checkboxElement.dataset.id = index;
 
     listElement.appendChild(checkboxElement);
     listElement.appendChild(labelElement);
 
     ulElement.append(listElement);
-    console.count("x");
   });
 
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
 
+function click(e) {
+  if (e.target.type === 'checkbox') {
+    todos[e.target.dataset.id].done = e.target.checked;
+  }
+  populateList(todos);
+}
+
+populateList(todos);
 formElement.addEventListener('submit', addTodo);
+divElement.addEventListener('click', click);
