08c92e2d8f8db77db52f6b33f4db9d6d160a7dc5
briceyokoyama <briceyokoyama@gmail.com>
2019-11-13 01:13:24 -0800

    add slide in on scroll

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import warmUp from "./warmup";
 import clock from "./clock";
 import dropdown from "./drop_down";
 import todoList from "./todo_list";
+import slideScroll from "./slide_scroll"
--- src/slide_scroll.js
+++ src/slide_scroll.js
@@ -32,3 +32,21 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const images = document.querySelectorAll('img');
+
+function handleScroll(e) {
+  images.forEach(image => {
+    const slideInAt = (window.scrollY + window.innerHeight) - image.height/2;
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(handleScroll))
